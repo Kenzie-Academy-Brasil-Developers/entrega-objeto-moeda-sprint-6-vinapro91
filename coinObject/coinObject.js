14df508c19e820c1b39eb5d996daec40c4bab175
vinicius <vinapro91@gmail.com>
2021-08-19 17:55:01 -0300

    feat: corrigido conforme orientação da rubrica

--- coinObject/coinObject.js
+++ coinObject/coinObject.js
@@ -6,19 +6,14 @@ body.appendChild(main)
 const coin = {
     state: 0,
     flip: function () {
-      // Use "this.state" para acessar a propriedade "state".
       this.state = Math.floor(Math.random() * (1 - 0 + 1)) + 0
-      // Configure de forma randômica a propriedade “state” do
-      // seu objeto moeda. "STATE" deve receber somente os valores 0 ou 1.
       return this.state
     },
   
     toString: function () {
-      // Se o valor de "state" for 0, retorne "Heads"
       if (this.state === 0){
           return "Heads"
       }
-      // Se o valor de "state" for 1, retorne "Tails"
       if (this.state === 1){
         return "Tails"
     }
@@ -26,7 +21,6 @@ const coin = {
   
     toHTML: function () {
         const image = document.createElement("img");
-        // Colocar uma imagem correspondente a essa valor.
         if(this.state === 0){
         image.src = "./img/heads.jpg"
         image.alt = "Heads"
@@ -35,44 +29,32 @@ const coin = {
         image.src = "./img/Tail.jpg"
         image.alt = "Tail"
         }
-      // image.src = "./CAMINHO/IMAGEM.JPEG"
-      // image.alt = "Heads/Tails"
-      main.appendChild(image)
       return image;
     },
   };
 
   function display20Flips() {
     const results = [];
-    // Use um loop para arremessar a moeda 20 vezes.
+
         for(let i =0; i<=20;i++){
             coin.flip()
             results.push(coin.toString())
         }
     
-    main.textContent = results
-    
+    main.append(results) 
    return results
-    // Depois que o seu loop terminar, exiba o resultado na página no formato de TEXTO.
-    // Além de exibir os resultados na página, não esqueça
-    // de retornar o valor de "results".
-    // Caso esqueça de retornar "results", sua função não
-    // irá passar nos testes.
   }
   
   function display20Images() {
     const results = [];
-    // Use um loop para arremessar a moeda 20 vezes.
     for(let i =0; i<=20;i++){
         coin.flip()
         results.push(coin.toHTML())
     }
+    for(let i = 0; i<results.length; i++){
+      main.append(results[i])
+    }
     return results
-    // Depois que o seu loop terminar, exiba o resultado na página no formato de IMAGEM.
-    // Além de exibir os resultados na página, não esqueça
-    // de retornar o valor de "results".
-    // Caso esqueça de retornar "results", sua função não
-    // irá passar nos testes.
   }
   
   display20Flips()
